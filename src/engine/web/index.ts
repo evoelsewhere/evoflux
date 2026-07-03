0bf943e0bfff66d17b5216aa2b1f5d1ccddceda9
khuonghung <khuonghung1423@gmail.com>
2026-07-04 01:51:34 +0700

    chore: remove leftover old Electron/TS stack files after v2 replacement

--- src/engine/web/index.ts
+++
@@ -1,3 +0,0 @@
-export { searchWeb, type SearchResult } from './search'
-export { fetchPage, fetchPages, type FetchedPage } from './fetcher'
-export { parseHTML, type ParsedContent } from './parser'
